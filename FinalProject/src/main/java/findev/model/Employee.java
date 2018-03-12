2d381773f8cf126a278924ddf3c64112494d707c
Iaroslav <diachenkoiaroslav@gmail.com>
2018-03-12 17:04:38 +0200

    update

--- FinalProject/src/main/java/findev/model/Employee.java
+++ FinalProject/src/main/java/findev/model/Employee.java
@@ -3,9 +3,10 @@ package findev.model;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.hibernate.mapping.Set;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.math.BigDecimal;
 
 @Entity
@@ -19,12 +20,6 @@ public class Employee extends BaseEntity {
 
     @Column(name = "last_name")
     private String lastName;
-
-    @Column(name = "address")
-    private String address;
-
-    private Set Department;
-    private Set Status;
 
     @Column(name = "salary")
     private BigDecimal salary;
